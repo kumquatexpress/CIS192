bbefb5c4ad966faf3b839b47fcdd54a88c47ce1a
David Xu <xudavidb@gmail.com>
2013-04-06 22:22:59 -0400

    added static files, moved things around

--- app.py
+++ app.py
@@ -7,12 +7,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template('hierarchy.html')
+    return render_template('index.html')
 
 
 @app.route("/hierarchy")
 def hierarchy():
     return render_template('hierarchy.html')
+
+
+@app.route("/list")
+def list():
+    return render_template('list.html')
+
+
+@app.route("/new")
+def new():
+    return render_template('new.html')
+
+
+@app.route("/project")
+def project():
+    return render_template('project.html')
 
 
 if __name__ == "__main__":
