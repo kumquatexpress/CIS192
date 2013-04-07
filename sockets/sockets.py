4529f3aca723513f03d134ac0b90ee500c7b3bc4
Patrick Hulce <patrick.hulce@gmail.com>
2013-04-06 20:44:59 -0400

    Got sockets working

--- sockets/sockets.py
+++ sockets/sockets.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
-import socket, threading, time
+import socket, threading, time, re
+from base64 import b64encode
+from hashlib import sha1
 
 def handle(s,a):
   print a
-  print repr(s.recv(4096))
-  s.send('''
-HTTP/1.1 101 Web Socket Protocol Handshake\r
-Upgrade: WebSocket\r
-Connection: Upgrade\r
-WebSocket-Origin: http://localhost:8888\r
-WebSocket-Location: ws://localhost:9876/\r
-WebSocket-Protocol: sample
-  '''.strip() + '\r\n\r\n')
+  handshake_data = s.recv(4096)
+  client_key = re.search("Sec-WebSocket-Key:\s+(.*?)[\n\r]+", handshake_data).groups()[0].strip()
+  handshake_response = (
+    'HTTP/1.1 101 Web Socket Protocol Handshake',
+    'Upgrade: WebSocket',
+    'Connection: Upgrade',
+    'WebSocket-Location: ws://localhost:9876/',
+    'Sec-WebSocket-Accept: {hashed_key}\r\n\r\n'
+  )
+  WSID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+  server_key = b64encode(sha1(client_key + WSID).digest())
+  response = "\r\n".join(handshake_response).format(hashed_key=server_key)
+  print response
+  s.send(response)
   time.sleep(1)
-  s.send('\x00hello\xff')
+  #s.send('\x00hello\xff')
   time.sleep(1)
-  s.send('\x00world\xff')
-  s.close()
+  #s.send('\x00world\xff')
+  #s.close()
 
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
